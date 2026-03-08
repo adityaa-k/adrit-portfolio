6cd1ec22a5a9c82344d1c9cd0ca92434d1e30ad8
adkedv <aditya.kar@edvanta.com>
2026-03-08 23:23:58 +0530

    Remove diagnostic scripts that were breaking the build

--- scripts/test-yf.ts
+++
@@ -1,5 +0,0 @@
-import * as yahooFinanceModule from 'yahoo-finance2';
-console.log('Module keys:', Object.keys(yahooFinanceModule));
-if (yahooFinanceModule.default) {
-    console.log('Default keys:', Object.keys(yahooFinanceModule.default));
-}
